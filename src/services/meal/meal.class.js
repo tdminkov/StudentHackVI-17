a942df0319723d1756f32aef5abf52fa7d943876
Carlos <carlos.pinavazgomes@student.manchester.ac.uk>
2018-04-29 02:14:19 +0100

    Sexiest javascript map you aint ever seen

--- src/services/meal/meal.class.js
+++ src/services/meal/meal.class.js
@@ -1,7 +1,70 @@
 /* eslint-disable no-unused-vars */
 const mongo = require("mongodb").MongoClient;
-const uri = "mongodb://Admin:Admin@ds161939.mlab.com:61939/studenthack";
+const uri = "mongodb://Admin:Admin@ds161939.mlab.com:61939/studenthack"
 
+const tableFromUID = (uid, mealName) => {
+  return new Promise((resolve, reject) => {
+    let fedTable;
+    mongo.connect(uri, function (err, db) {
+      if (err) {
+        throw err
+      }
+
+      db.collection("meals").findOne({
+        mealName: mealName
+      }, {}, (error, result) => {
+        if (error) {
+          throw error
+        } else {
+          console.log(result)
+          fedTable = result.invited_tables.find((element) => {
+            return element.table_meal_UID === uid.table_meal_UID
+          })
+
+          db.close()
+          resolve(fedTable)
+        }
+      })
+    })
+  })
+}
+
+const tablesToInvite = (mealName) => {
+  return new Promise((resolve, reject) => {
+    let invitedTables;
+    let allTables;
+  
+    mongo.connect(uri, function (err, db) {
+      if (err) {
+        reject(err)
+      }
+  
+      db.collection("tables").find().toArray((error, result) => {
+        if (error) {
+          reject(error)
+        } else {
+          allTables = result;
+        }
+      })
+  
+      db.collection("meals").findOne({
+        mealName: mealName
+      }, {
+        invited_tables: 1,
+        _id: 0
+      }, (error, result) => {
+        if (error) {
+          reject(error)
+        } else {
+          invitedTables = result.invited_tables;
+        }
+        db.close();
+        const returnable = Array.from(allTables.filter(x => !invitedTables.map(elem => elem.tableNo).includes(x.tableNo))).slice(0, 2)
+        resolve(returnable)
+      })
+    })
+  })
+}
 
 class Service {
   constructor(options) {
@@ -61,76 +124,76 @@ class Service {
           invited_tables: [],
           served_tables: []
         }, {}, (error, result) => {
+          db.close()
           if (error) {
             reject(error)
           } else {
-            resolve()
+            resolve("Great Success!")
           }
         })
-        db.close()
       })
     })
   }
 
   update(mealName, data, params) {
     //params mode=invite or feed
-
     return new Promise((resolve, reject) => {
-
-
-      mongo.connect(uri, function (err, db) {
-
-        if (err) {
-          reject(err)
-        }
-
-        let mode = params.query.mode;
-        if (mode == "invite") {
-
-          let newTables = data.new_tables;
-          db.collection("meals").update({
-              mealName: mealName
-            }, {
-              $push: {
-                invited_tables: { $each: newTables }
+        mongo.connect(uri, async function (err, db) {
+            if (err) {
+              reject(err)
+            }
+            let mode = params.query.mode;
+            if (mode == "invite") {
+              const servedTable = tablesToInvite(mealName).then(names => {
+                  const invitedTables = names.map(elem => {return {tableNo: elem.tableNo, UID: 3}})
+                  db.collection("meals").update({
+                      mealName: mealName
+                    }, {
+                      $push: {
+                        invited_tables: {
+                          $each: invitedTables
+                        }
+                      }
+                    },
+                    function (err, document) {
+                      resolve("Great Success!")
+                      db.close()
+                    })
+                })
               }
-            },
-            function (err, document) {
-              resolve()
+              else if (mode == "feed") {
+                let uid = data.feed;
+                const servedTable = tableFromUID(uid, mealName).then(table => {
+                  db.collection("meals").update({
+                      mealName: mealName
+                    }, {
+                      $push: {
+                        served_tables: table
+                      }
+                    },
+                    function (err, document) {
+                      resolve("Great Success!")
+                      db.close()
+                    })
+                })
+              }
             })
-          
-          }
-          else if (mode == "feed") {
-            let newTable = data.feed;
-            db.collection("meals").update({
-                mealName: mealName
-              }, {
-                $push: {
-                  served_tables: newTable
-                }
-              },
-              function (err, document) {
-                resolve()
-              })
-          }
-          db.close()
-      })
-    })
+        })
+    }
+
+    patch(id, data, params) {
+      return Promise.resolve(data);
+    }
+
+    remove(id, params) {
+      return Promise.resolve({
+        id
+      });
+    }
   }
 
-  patch(id, data, params) {
-    return Promise.resolve(data);
-  }
+  module.exports = function (options) {
+    return new Service(options);
+  };
 
-  remove(id, params) {
-    return Promise.resolve({
-      id
-    });
-  }
-}
-
-module.exports = function (options) {
-  return new Service(options);
-};
-
-module.exports.Service = Service;
+  module.exports.Service = Service;
